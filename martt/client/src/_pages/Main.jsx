3bd55dc87edca6390d766db5d7c4579ca4eaea67
Jawad <jawadal829@gmail.com>
2024-10-25 14:36:47 +0500

    updated

--- martt/client/src/_pages/Main.jsx
+++ martt/client/src/_pages/Main.jsx
@@ -11,6 +11,7 @@ const Main = () => {
   const { id } = useParams();
   const [data, setData] = useState({});
   const [selectedImage, selectImage] = useState();
+  const [sizePop, setSizePop] = useState(false);
   const fetchResponse = async () => {
     try {
       const res = await axios.get(`${productApi}/${id}`);
@@ -31,13 +32,13 @@ const Main = () => {
     <div className="main-container flex col">
       <div className="product-info flex">
         <div className="left flex col">
-          <div className="shop-info flex col">
+          {/* <div className="shop-info flex col">
             <span>{data?.shop?.name}</span>
             <p>{data?.shop?.description}</p>
             <p>{data?.shop?.location}</p>
             <p>{data?.shop?.contactNumber}</p>
             <p>{data?.shop?.email}</p>
-          </div>
+          </div> */}
           <div className="cat-info flex col">
             <span>{data?.category}</span>
             <p>
@@ -128,11 +129,27 @@ const Main = () => {
             ) : (
               this
             )}
-            <button className="btn-buy flex">NEXT</button>
+            <button
+              className="btn-buy flex"
+              onClick={() => (size ? alert("working") : setSizePop(true))}
+            >
+              NEXT
+            </button>
           </div>
         </div>
       </div>
       <Latest productLength={4} />
+      {sizePop && (
+        <div className="size-warning flex" onClick={() => setSizePop(false)}>
+          <div className="card flex col" onClick={(e) => e.stopPropagation()}>
+            <h2>warning</h2>
+            <p>you must select a size & color color is black by default.</p>
+            <button className="btn-close" onClick={() => setSizePop(false)}>
+              close
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
